f8315bb69a2ca845dad0980b8d2f8b4a207cac99
James Dale <git@jamesdale.com.au>
2018-02-02 12:07:35 +1100

    Add tests

---
+++ test/TestBootstrap.js
@@ -0,0 +1,18 @@
+/**
+ * messagemedia-lookups
+ *
+ * This file was automatically generated for MessageMedia by APIMATIC v2.0 ( https://apimatic.io )
+ */
+ 'use strict';
+
+const configuration = require("../lib/configuration");
+
+const testConfiguration = {
+    ASSERT_PRECISION: 0.01,
+    TEST_TIMEOUT: 30000
+};
+
+configuration.basicAuthUserName = process.env.MessageMediaApiTestsKey;
+configuration.basicAuthPassword = process.env.MessageMediaApiTestsSecret;
+
+module.exports = testConfiguration;
