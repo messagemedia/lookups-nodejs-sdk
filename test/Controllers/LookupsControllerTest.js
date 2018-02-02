c1b7146d86dfc54b75d4148dd9977af0b444e736
James Dale <git@jamesdale.com.au>
2018-02-02 15:28:41 +1100

    Remove string for assert

--- test/Controllers/LookupsControllerTest.js
+++ test/Controllers/LookupsControllerTest.js
@@ -59,7 +59,7 @@ Each property in the response body is defined as follows:
             headers['Content-Type'] = null;
             assert.isTrue(TestHelper.areHeadersProperSubsetOf(headers, context.response.headers, true));
             assert.isNotNull(response);
-            assert.equal('{"countryCode":"AU","phoneNumber":"+61491570156","type":"MOBILE","carrier":{"name":"AU Landline Carrier"}}', context.response.body);
+            assert.equal({"countryCode":"AU","phoneNumber":"+61491570156","type":"MOBILE","carrier":{"name":"AU Landline Carrier"}}, context.response.body);
             done();
         });
     });
